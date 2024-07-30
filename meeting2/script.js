0a140655cc49721ed2f8ae8436159d2b7607afc7
Taqiyuddin <160695552+IsTeqiyou@users.noreply.github.com>
2024-07-30 11:20:25 +0700

    meeting2

---
+++ meeting2/script.js
@@ -0,0 +1,21 @@
+let tanpaNilai;
+console.log(tanpaNilai);
+alert(tanpaNilai)
+
+let nama = "Taqi";
+let umur = 15;
+let tempatTinggal = "Babel";
+let kalimatPerkenalan = "Nama saya adalah " + nama + ", umur saya " + umur + " tahun, dan saya tinggal di " + tempatTinggal + ".";
+console.log(kalimatPerkenalan);
+alert (kalimatPerkenalan);
+
+let nilaiNull = null;
+console.log(nilaiNull);
+alert(nilaiNull)
+
+
+let alas = 5;
+let tinggi = 7;
+let luasSegitiga = 0.5 * alas * tinggi;
+console.log("Luas segitiga adalah: " + luasSegitiga);
+alert(luasSegitiga)
